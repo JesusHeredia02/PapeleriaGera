5d1b17a2b8bb148344c85293753d59407e1ae40f
Luis Gerardo <arturopachecomote20ajpacheco20@gmail.com>
2023-12-06 18:13:00 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import productoRouter from "./routes/productos.routes.js"
 import clienteRouter from "./routes/clientes.routes.js"
 import ventasRouter from "./routes/ventas.routes.js"
+import cors from 'cors'
 import './config.js'
 //import connectionRouter from "./routes/connection.routes.js"
 
@@ -12,7 +13,9 @@ const app = express();
 
 
 app.use(express.json())
+app.use(cors());
 app.use('/api',productoRouter)
 app.use('/api',clienteRouter)
+app.use(cors())
 app.use('/api',ventasRouter)
 export default app;
